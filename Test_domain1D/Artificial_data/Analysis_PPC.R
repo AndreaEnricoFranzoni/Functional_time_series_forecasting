13ee14972c20d14bb9170230f1db1f3fb9e64a05
Andrea Enrico Franzoni <andreaenrico.franzoni@gmail.com>
2025-01-14 20:38:14 +0100

    Corrected some plots

--- Test_domain1D/Artificial_data/Analysis_PPC.R
+++ Test_domain1D/Artificial_data/Analysis_PPC.R
@@ -47,7 +47,7 @@ format_file = ".png"  #and, in case, the format
 
 #reg param that have been tested during the predictions
 alpha_tested = c(10^(-10),10^(-9),10^(-8),10^(-7),10^(-6),10^(-5),10^(-4),10^(-3),10^(-2),10^(-1),
-                 1,10,10^2,10^3,10^4,10^5,10^6,10^7,10^8,10^9,10^10,10^11)
+                 1,10,10^2,10^3,10^4,10^5,10^6,10^7,10^8,10^9,10^10)
 #nPPCs tested during the predictions
 k_test = 1:number_pred_sing
 
